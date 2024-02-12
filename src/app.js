9233ec6ed2788d786e1c589cf6a62d07bff9c6ca
EzeTxle <60270488+EzeTxle@users.noreply.github.com>
2024-02-12 18:03:53 -0300

    desafio

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import { ProductManager } from './ProductManager.js';
+import express from 'express'
+
+const port = 8080;
+const productManager = new ProductManager("./idk.json");
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", async (req, res) => {
+    res.send("Working");
+});
+app.get("/products", async (req, res) => {
+    let limit = req.query.limit;
+    let productArray = productManager.getProducts();
+    if(isNaN(limit)) return res.send(productArray);
+    let limitArray = productArray.slice(0,Number(limit));
+    res.send(limitArray);
+});
+app.get("/products/:pid", async (req, res) => {
+    let id = req.params.pid;
+    try{
+        let product = await productManager.getProductById(id);
+        res.send(product);
+    } catch (error) {
+        res.send(error.toString());
+    }
+    
+    
+});
+
+app.listen(port, () => console.log("running"))
